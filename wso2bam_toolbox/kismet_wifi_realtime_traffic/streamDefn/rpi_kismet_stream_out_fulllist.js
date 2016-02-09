3e26cc3d866912d4ed89da13773a881f39f1f667
Chilcano <chilcano@intix.info>
2016-02-09 18:11:02 +0100

    WSO2BAM toolbox (dashboard) and Stream, Exec Plan, Formatters definitions for BAM/CEP added

---
+++ wso2bam_toolbox/kismet_wifi_realtime_traffic/streamDefn/rpi_kismet_stream_out_fulllist.js
@@ -0,0 +1,38 @@
+{
+  "name": "rpi_kismet_stream_out_fulllist",
+  "version": "1.0.0",
+  "nickName": "rpi_k_out_fulllist",
+  "description": "Kismet out stream - Full list",
+  "metaData": [
+    {
+      "name": "bssid",
+      "type": "STRING"
+    }
+  ],
+  "payloadData": [
+    {
+      "name": "type",
+      "type": "STRING"
+    },
+    {
+      "name": "channel",
+      "type": "STRING"
+    },
+    {
+      "name": "llcpackets",
+      "type": "STRING"
+    },
+    {
+      "name": "datapackets",
+      "type": "STRING"
+    },
+    {
+      "name": "macAddress",
+      "type": "STRING"
+    },
+    {
+      "name": "signal_dbm",
+      "type": "STRING"
+    }
+  ]
+}
---
+++ wso2bam_toolbox/kismet_wifi_realtime_traffic/streamDefn/rpi_kismet_stream_out_totalpackets.js
@@ -0,0 +1,12 @@
+{
+  "name": "rpi_kismet_stream_out_totalpackets",
+  "version": "1.0.0",
+  "nickName": "rpi_k_out_totalpackets",
+  "description": "Kismet out stream - Total packets",
+  "payloadData": [
+    {
+      "name": "total_packets",
+      "type": "LONG"
+    }
+  ]
+}
